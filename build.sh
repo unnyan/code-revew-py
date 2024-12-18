c507b4e572e20f481daa6abac084914fe45317a3
anna.didyk <anytacaramel@gmail.com>
2024-12-19 01:08:18 +0500

    Изменен build.sh

--- build.sh
+++ build.sh
@@ -1,20 +1,3 @@
 #!/bin/bash
 
-set -e
-
-echo "\nЗапуск сборки проекта...\n"
-
-echo "Запускаем docker-compose..."
-docker-compose down --remove-orphans
-docker-compose up --build -d
-
-echo "\nОжидание запуска базы данных...\n"
-sleep 10
-
-echo "\nВыполняем подготовительные скрипты...\n"
-docker exec -it recipe-diet-bot-app-1 python site_parser.py
-
-echo "\nЗапуск Telegram-бота...\n"
-docker exec -it recipe-diet-bot-app-1 python bot.py
-
-echo "\nПроект успешно собран и запущен!\n"
+docker-compose up --build
